fc1e69ce0088510306f38991b5f1d2f786b016f6
Daniel Ibanez <dibanezalegria@yahoo.com>
2019-01-25 10:51:35 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require('cors');
+const morgan = require('morgan');
+
+const index = require('./routes/index');
+const hello = require('./routes/hello');
+
+const app = express();
+const port = 1337;
+
+app.use(cors());
+
+// don't show the log when it is test
+if (process.env.NODE_ENV !== 'test') {
+    // use morgan to log at command line
+    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
+}
+
+
+app.use('/', index);
+app.use('/hello', hello);
+
+
+
+
+// Add routes for 404 and error handling
+// Catch 404 and forward to error handler
+// Put this last
+app.use((req, res, next) => {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+
+// Start up server
+app.listen(port, () => console.log(`Example API listening on port ${port}!`));
